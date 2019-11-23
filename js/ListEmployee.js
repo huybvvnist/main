5a0be148830120e59c38d7066fe03499dd3d2870
hungcuong17 <cuongth.vnist@gmail.com>
2019-11-23 08:32:10 +0700

    update

--- js/ListEmployee.js
+++ js/ListEmployee.js
@@ -1,19 +1,3 @@
 $(function () {
-    $('.list').DataTable({
-      'paging'      : true,
-      'lengthChange': true,
-      'searching'   : true,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : false
-    })
-    // thiết lập tính năng bảng danh sách khoá học bắt buộc
-    $('.listcourse').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : true,
-      'info'        : false,
-      'autoWidth'   : false
-    })
-  })
+    $('.select2').select2()
+})
